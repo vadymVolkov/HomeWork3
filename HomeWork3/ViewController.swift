f25c1db428c76377cc0d13201ea690b9232af03b
Vadym <volkov.vadym@gmail.com>
2020-05-07 21:44:21 +0300

    Initial Commit

---
+++ HomeWork3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork3
+//
+//  Created by Vadym Volkov on 07.05.2020.
+//  Copyright © 2020 Vadym Volkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
